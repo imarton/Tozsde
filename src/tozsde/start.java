534e6828aaa013ea31ef1d24e64112f09f8d5903
martoni <martoni@martoni2-PC>
2014-02-01 17:42:17 +0100

    Az alapvető funkciókat tartalmazza. Kiindulási állapotnak megfelelő.

--- src/tozsde/start.java
+++ src/tozsde/start.java
@@ -2,6 +2,10 @@ package tozsde;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -20,9 +24,33 @@ import java.util.logging.Logger;
  */
 public class start {
     public static void main(String[] args) {
-        //teszt
- 
-        
+        try {
+            //teszt
+            ChartDataManager cman = new ChartDataManager(parseFile(new File("F:\\munka\\PowerCharts_XT_Eval\\Tozsde\\data\\nyers\\20140131.txt")));
+            
+            FileOutputStream fos = new FileOutputStream("F:\\munka\\PowerCharts_XT_Eval\\Tozsde\\data\\Tozsde1.xml");
+            OutputStreamWriter osw = null;
+            try {
+                osw = new OutputStreamWriter(fos, "UTF-8");
+                osw.write(cman.createChart().makeXML());
+                //System.out.println(cman.createChart().makeXML());
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(start.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(start.class.getName()).log(Level.SEVERE, null, ex);
+            }finally{
+                if(osw != null)
+                    try {
+                        osw.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(start.class.getName()).log(Level.SEVERE, null, ex);
+                }                
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(start.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println(" -- KÉSZ -- ");
     }
     /**
      * Adat forrás egy sorát alakítja át PowerChart XMLnek megfelelő alakúra
@@ -42,9 +70,9 @@ public class start {
             Date d = df.parse(datestr);
             trans = new Trans();
             trans.name = data[0]; //name - "OTP"
-            trans.time = d; //time - yyyyMMdd HHmmss
-            trans.price = data[4]; //price - 4500
-            trans.amount = data[5]; //amount - 100
+            trans.time = d; //time - yyyy.MMdd HHmmss
+            trans.price = Integer.parseInt(data[4]); //price - 4500
+            trans.amount = Integer.parseInt(data[5]); //amount - 100
 
 //        } catch (ParseException e) {  
 //            System.out.println("Sor kihagyva! Hiba a dátum konverziója közben: " + e.getLocalizedMessage());
@@ -62,20 +90,20 @@ public class start {
     private static ArrayList<Trans> parseFile(File file) throws FileNotFoundException{
         ArrayList<Trans> elemList = new ArrayList<>();
         Scanner sc = new Scanner(file);
+//        int i = 0;
         while (sc.hasNextLine()) {
+//            i++;
             String line = sc.nextLine();
             try {
-                elemList.add(parseLine(line));
+                Trans t = parseLine(line);
+//                System.out.format("%d. %s\n", i, t);
+                elemList.add(t);
             } catch (ParseException ex) {
                 Logger.getLogger(start.class.getName()).log(Level.SEVERE, "Sor kihagyva! " + line, ex);
             }
         }
         return elemList;
-    }
-    
-    private static void createXMLData(ArrayList<Trans> transList){
-        
-    }
+    }    
 }
 /**
  * Tranzakciók tárolását szolgáló osztály
@@ -86,8 +114,15 @@ public class start {
 class Trans{
     String name;
     Date time;
-    String price;
-    String amount;
+    int price;
+    int amount;
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        return String.format("%s: %s.- (%s db)", sf.format(time), price, amount);
+    }
+    
     
 }
 
@@ -115,7 +150,7 @@ class ChartDataManager{
      * azaz a gyertya ebben az időintervallumban lévő adatok alapján kerül majd kiszámolásra a nextSet()
      * metódus segítségével.
      */
-    private Date beginofTimeWindow;
+    private Date startTime;
     
     /**
      * Candle period time in second
@@ -123,12 +158,6 @@ class ChartDataManager{
      */
     private int periodTime = 1*60; 
     
-    /**
-     * A grafikon milyen időintervallumbeli adatok jelenjenek meg
-     * Ez egy szűkítési lehetőség, ha nincs megadva az összes adat megjelenik
-     */
-    private Date startTime;
-    private Date endTime;
 
     // chart grafikus jellemzői
     private short numPDivLines = 5;
@@ -142,6 +171,14 @@ class ChartDataManager{
     private String VYAxisName = "Mennyiség";
     
     public ChartDataManager(ArrayList<Trans> datas) {
+        
+        try {
+            this.startTime = (new SimpleDateFormat("yyyy.MM.dd HH:mm")).parse("2014.01.31 09:00");
+        } catch (ParseException ex) {
+            Logger.getLogger(ChartDataManager.class.getName()).log(Level.SEVERE, null, ex);
+            this.startTime = new Date();
+        }
+        
         if(datas != null)
             for (Trans t : datas) { //adatok bemásolása az osztály saját listájába
                 this.datas.add(t);
@@ -163,26 +200,26 @@ class ChartDataManager{
         DataSet ds = new DataSet();
         chart.addChild(ds);
         
-        this.resetPosition();
-        while(this.hasNextSet()){
-            ds.addChild(this.nextSet());
+        this.resetPosition(); // chart gyertyáinak kiszámítsa előtt lapahelyzetbe állítjuk az adatok feldolgozottságát jelző mutatót.
+        while(this.hasNextSet()){ // chart gyertyáinak kiszámítsa 
+            Set s = this.nextSet();
+            if(!s.empty())
+                ds.addChild(s);
         }
+        
         
         Categories cats = new Categories();
         chart.addChild(cats);
-        
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy.MM.dd HH:mm");
         Category cat = new Category();
-        cat.label = "9:00";
+        cat.label = sf.format(chart.getStartTime());
         cat.x = 0;
         cats.addChild(cat);
         
         cat = new Category();
-        cat.label = "17:15";
-        cat.x = 30;
+        cat.label = sf.format(chart.getEndTime());
+        cat.x = chart.getNumberOfCandle();
         cats.addChild(cat);                
-        
-        
-        
         
         return chart;
     }
@@ -210,36 +247,53 @@ class ChartDataManager{
      * 
      * @return end time of candle stick
      */
-    private Date getEndofTimeWindow(){
-        return new Date(this.beginofTimeWindow.getTime() + this.periodTime*1000 );
+    private Date getEndTime(){
+        return new Date(this.startTime.getTime() + this.periodTime*1000 );
     }
     
     /**
-     * Megvizsgálja hogy a rendelkezésre álló kereskedési adatok alapján 
+     * Megvizsgálja hogy a rendelkezésre álló, még fel nem dolgozott kereskedési adatok alapján 
      * ki lehet-e számolni a következő Gyertyát.
-
+     * <br> Használd a nextSet() metódust a Gyertya elem előállításához.
      * 
-     * @return 
+     * @return előállítható a a következő Gyertya elem 
      */
     private boolean hasNextSet() {
-        if(lastPosition < this.datas.size())
-            return false;
-        if(this.datas.get(datas.size()-1).time.before(this.getEndofTimeWindow()) )
+        //ha nincs feldolgozatlan adat a listában
+        if(lastPosition >= this.datas.size())
             return false;
         return true;
+//        //meghatározzuk az utolsó kereskedési adat idejét
+//        //majd eldöntjük, hogy ez az időpont az aktuális Gyerta által lefedett időintervallum záró ideje ELŐTT van-e
+//        // - ha igen, akkor hamissal térünk vissza ugyanis ez azt jelenti, hogy nincs a Gyerta befejezéséhez elegendő adat
+//        // - ha nem, akkor igazzal térünk vissza
+//        return !this.datas.get(datas.size()-1).time.before(this.getEndTime());
     }
 
-    private ChildElem nextSet() {
+    /**
+     * A feldolgozatlandatokból  - amelyek a megfelelő időintervallumban vannak - 
+     * előállít egy Gyertya elemet.
+     * @return 
+     */
+    private Set nextSet() {
         if(!this.hasNextSet())
            throw new NoSuchElementException();
         
-        Set set = new Set();        
-        while(true){
-            if(this.lastPosition < this.datas.size())
-                throw new IndexOutOfBoundsException(String.format("Last Position (%d) > data.size (%d)",this.lastPosition, this.datas.size()));            
+        Set set = new Set(); 
+//        SimpleDateFormat sf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+//        System.out.format("Set start -- start time: %s, start pos: %d, end time: %s  \n",sf.format(startTime), lastPosition, sf.format(getEndTime()) );
+//        int i = 0;
+        while(lastPosition < this.datas.size()){
+            if(this.datas.get(lastPosition).time.after(this.getEndTime())) //ha az aktuális pozicíóban lévő adat már nincs benne az aktuális gyertya intervallumában
+                break;
+//            i++;
+//            System.out.println("\t"+i + "." +datas.get(lastPosition));
+            set.add(this.datas.get(lastPosition));
+            lastPosition++;
         }
-        
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.startTime = this.getEndTime();
+//        System.out.println("Set end -- " + set);
+        return set;
     }
 
     private void resetPosition() {
@@ -254,18 +308,148 @@ class ChartDataManager{
  * @author martoni
  */
 class Set extends ChildElem{
-    public Integer open;
+    /**
+     * A gyertya által lefedett időintervallum nyitó ideje
+     */
+    private Date startTime;
+    /**
+     * A gyertya által lefedett időintervallum záró ideje
+     */
+    private Date endTime;
+    
+    /**
+     * Nyitóár
+     */
+    public Integer open; 
+    /**
+     * Záróár
+     */
     public Integer close;
-    public Integer hight;
+    /**
+     * Legmagasabb ár
+     */
+    public Integer high;
+    /**
+     * Legalacsonyabb át
+     */
     public Integer low;
+    /**
+     * Tranzakciók összértéke
+     */
     public Integer volume;
-    public Integer x; //Egy sorszám: The x-axis value for the plot. The candlestick point will be placed horizontally on the x-axis based on this value.
-    public String valuetext; //érték szöveg megjelenítése
-    public String color; //hexa érték
+    /**
+     * Egy sorszám: The x-axis value for the plot. The candlestick point will be placed horizontally on the x-axis based on this value.
+     */
+    public Integer x;
+    /**
+     * A charton a gyertya fölött megjelenithető érték szöveg 
+     */
+    public String valuetext; 
+    /**
+     * Gyertya színe: kitöltés
+     * hexa érték
+     */
+    public String color;
+    /**
+     * Gyertya színe: körvonal
+     * hexa érték
+     */
     public String bordercolor;
     
+    
+    /**
+     * A tranzakció adatait beépíti a gyertyába
+     * @param t Tranzakció
+     */
+    public void add(Trans t){
+        if(this.open == null){ //az első tranzakció árfolyama a nyitó ár és annak időpontja
+            this.open = t.price; 
+            this.startTime = t.time;
+            this.volume = 0;
+            this.high = t.price;
+            this.low = t.price;
+        }
+        
+        this.close = t.price;  // a záróárba is mindig eltároljuk a tranzakció árát, így a végén az utólsó tranzakció ára lesz benne.
+        this.endTime = t.time; // hasonlóan az időt is
+        
+        if(t.price > this.high)
+            this.high = t.price;
+        if(t.price < this.low)
+            this.low = t.price;
+        
+        this.volume += t.amount;
+    }
+
+//    /**
+//     * Beállítja a gyertya által lefedett időintervallum kezdetét
+//     * @param endTime 
+//     */
+//    public void setEndTime(Date endTime) {
+//        this.endTime = endTime;
+//    }
+//    /**
+//     * Beállítja a gyertya által lefedett időintervallum végét
+//     * @param startTime 
+//     */
+//    public void setStartTime(Date startTime) {
+//        this.startTime = startTime;
+//    }
+    
+    
+    /**
+     * Visszaadja a gyertya által lefedett időintervallum végét
+     * @return 
+     */
+    public Date getEndTime() {
+        return endTime;
+    }
+    /**
+     * Visszaadja a gyertya által lefedett időintervallum kezdetét
+     * @return 
+     */
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    @Override
+    public String toString() {
+        if(this.empty())
+            return "empty set";
+        
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        return String.format("Open-Close: %d-%d; High-Low: %d-%d; Start-End time: %s - %s; Amount: %d", open, close, high, low, sf.format(startTime), sf.format(endTime), volume );
+        
+    }    
+
+    public boolean empty() {
+        return this.open == null;
+    }
 }
-class DataSet extends ChildElem{    
+class DataSet extends ChildElem{  
+
+    @Override
+    public void addChild(ChildElem elem) {
+        ((Set)elem).x = this.getNumberOfChildren()+1;
+        super.addChild(elem);         
+    }
+    /**
+     * Visszaadja az első gyertya elemet.     
+     * @return az első Gyertya elem illetve null ha nincs eleme a DataSet-nek.
+     */
+    protected Set getFirstCandle() {        
+        return this.getNumberOfChildren()==0?null:(Set)this.getChild(0);
+    }
+
+    
+    /**
+     * Visszaadja az utolsó gyertya elemet.     
+     * @return az utolsó Gyertya elem illetve null ha nincs eleme a DataSet-nek.
+     */
+    protected Set getLastCandle() {
+        return this.getNumberOfChildren()==0?null:(Set)this.getChild(this.getNumberOfChildren()-1);
+    }
+    
 }
 
 
@@ -273,7 +457,7 @@ class Categories extends ChildElem{
 }
 class Category extends ChildElem{
     public String label;
-    public Short x;
+    public Integer x;
 }
 
 class Chart extends ChildElem{
@@ -286,6 +470,39 @@ class Chart extends ChildElem{
     public String PYAxisName;
     public String VYAxisName;
     public Byte volumeHeightPercent; 
+
+    /**
+     * Chartban lévő Gyertyák száma
+     * @return 
+     */
+    protected int getNumberOfCandle() {        
+        //megkeressük a Chart DataSet típusú elemét és lekérdezzük az abban lévő gyermekek (azaz Gyertyák) számát
+        DataSet ds = this.getDataSet();
+        return ds==null?0:ds.getNumberOfChildren();
+    }
+    
+    /**
+     * visszaadja a Chart-hoz tartozó DataSet-t (gyertyák halmaza)
+     * @return 
+     */
+    private DataSet getDataSet(){
+        Iterator<ChildElem> it = this.childList();
+        while(it.hasNext()){
+            ChildElem ce = it.next();
+            if(ce instanceof DataSet){                
+                return ((DataSet)ce);
+            }
+        }
+        return null;
+    }
+
+    public Date getStartTime() {
+        return this.getDataSet().getFirstCandle().getStartTime();
+    }
+
+    public Date getEndTime() {
+        return this.getDataSet().getLastCandle().getEndTime();
+    }
 }
 
 
@@ -298,6 +515,7 @@ class ChildElem {
         return this.makeXML("");
     }
     private String makeXML(String ident) {
+        //System.out.println(ident + this.getClass().getSimpleName());
         StringBuilder sb = new StringBuilder();
         sb.append(ident).append("<").append(this.getClass().getSimpleName().toLowerCase());
         Field[] fields = this.getClass().getFields();
@@ -327,6 +545,18 @@ class ChildElem {
     
     public Iterator<ChildElem> childList(){
         return this.children.iterator();
+    }
+    
+    protected ChildElem getChild(int index){
+        return this.children.get(index);
+    }
+    
+    /**
+     * az adott elemhez tartozó gyermekelemek száma
+     * @return 0 vagy nagyobb
+     */
+    public int getNumberOfChildren(){
+        return this.children.size();
     }
     
     public void addChild(ChildElem elem){
